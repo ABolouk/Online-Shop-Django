f1e76b949a5d00edee5ffefe8e57514b88b64dd6
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-14 21:10:17 +0330

    Added Product to product app

--- online_shop/product/models.py
+++ online_shop/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    price = models.CharField(max_length=20)
+    detail = models.TextField()
+    is_available = models.BooleanField(default=True)
+    # discount = models.ManyToManyField(to="Discount")
+    # category = models.ManyToManyField(to="Category")
