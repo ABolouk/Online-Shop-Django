0f17c49a651bfd00ad2b2d1cc1cc5ec7a9eef9c0
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-14 22:02:38 +0330

    added full_name() and __str__ to customer

--- online_shop/customer/models.py
+++ online_shop/customer/models.py
@@ -6,6 +6,12 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField()
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
 
 class Address(models.Model):
     province = models.CharField(max_length=100)
