d756edc7af203bf9925a9f3d4f863c9170f5ad9d
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-14 21:03:14 +0330

    added Customer

--- online_shop/customer/models.py
+++ online_shop/customer/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
