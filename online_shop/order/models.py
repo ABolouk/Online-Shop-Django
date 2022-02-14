a1ff1c3b786f99d0c87f54d4de083f591112c42a
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-14 21:31:46 +0330

    Bugfix: Order and OrderItem

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -6,11 +6,11 @@ from customer.models import Customer
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(to="Customer", on_delete=models.CASCADE)
+    customer = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
     discount_code = models.ForeignKey(to=DiscountCode, null=True, on_delete=models.SET_NULL)
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(to="Order", on_delete=models.CASCADE)
-    product = models.ForeignKey(to="Product", on_delete=models.CASCADE)
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     number = models.IntegerField(validators=[MinValueValidator(1), ])
