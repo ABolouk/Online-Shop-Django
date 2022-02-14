2eba10c5ccbd413db5f8a6f38ff8a4638aa5fb6b
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-14 21:03:40 +0330

    added Address

--- online_shop/customer/models.py
+++ online_shop/customer/models.py
@@ -5,3 +5,10 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField()
+
+
+class Address(models.Model):
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    details = models.TextField()
+    customer = models.ForeignKey(to="Customer", on_delete=models.CASCADE)
