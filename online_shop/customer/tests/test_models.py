f58d60cb4a5b2e629b7f9a094ae744c5f11dff88
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-14 22:00:13 +0330

    Added CustomerTest

--- online_shop/customer/tests/test_models.py
+++ online_shop/customer/tests/test_models.py
@@ -1,4 +1,16 @@
 from django.test import TestCase
 from customer.models import Customer, Address
+from datetime import date
 
 
+class CustomerTest(TestCase):
+    def setUp(self) -> None:
+        self.customer1 = Customer.objects.create(first_name="amir",
+                                                 last_name="bolouk",
+                                                 birth_date=date(1997, 5, 21))
+        self.customer2 = Customer.objects.create(first_name="akbar",
+                                                 last_name="babaii",
+                                                 birth_date=date(1961, 11, 2))
+
+    def test1_address1_(self):
+        pass
