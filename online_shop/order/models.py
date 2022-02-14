97d0093b5e1c3c3c5df726c8d8b80d33de1c3706
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-14 21:30:03 +0330

    Added OrderItem to order app

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 from product.models import Product, DiscountCode
 from customer.models import Customer
 
@@ -6,3 +8,9 @@ from customer.models import Customer
 class Order(models.Model):
     customer = models.ForeignKey(to="Customer", on_delete=models.CASCADE)
     discount_code = models.ForeignKey(to=DiscountCode, null=True, on_delete=models.SET_NULL)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(to="Order", on_delete=models.CASCADE)
+    product = models.ForeignKey(to="Product", on_delete=models.CASCADE)
+    number = models.IntegerField(validators=[MinValueValidator(1), ])
