25e20b624110e2eee8d7c9059e09622ebdcf1429
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-14 21:26:44 +0330

    Added Order to order app

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from product.models import Product, DiscountCode
+from customer.models import Customer
 
-# Create your models here.
+
+class Order(models.Model):
+    customer = models.ForeignKey(to="Customer", on_delete=models.CASCADE)
+    discount_code = models.ForeignKey(to=DiscountCode, null=True, on_delete=models.SET_NULL)
