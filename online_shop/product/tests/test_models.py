d9c467274b8f19aa78577dd6d98daee04b1bf0ee
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-15 11:13:48 +0330

    Added CategoryTest

--- online_shop/product/tests/test_models.py
+++ online_shop/product/tests/test_models.py
@@ -13,3 +13,26 @@ class DiscountTest(TestCase):
         self.assertFalse("per" == self.discount.type)
         self.assertEqual("40", self.discount.amount)
         self.assertEqual("20000", self.discount.max_value)
+
+
+class CategoryTest(TestCase):
+    def setUp(self) -> None:
+        self.category1 = Category.objects.create(name="Self Care",
+                                                 description="Medicinal and Self-Care products.")
+        self.category2 = Category.objects.create(name="Electronics",
+                                                 description="Consists of every electronic device ever.")
+        self.category3 = Category.objects.create(name="Laptop",
+                                                 description="Laptops",
+                                                 category=self.category1)
+        self.category4 = Category.objects.create(name="Smart Phone",
+                                                 category=self.category1)
+
+# class ProductTest(TestCase):
+#     def setUp(self) -> None:
+#         self.discount1 = Discount.objects.create(type="VAL",
+#                                                  amount="10000")
+#         self.discount2 = Discount.objects.create(type="PER",
+#                                                  amount="25")
+#         self.discount3 = Discount.objects.create(type="PER",
+#                                                  amount="40",
+#                                                  max_value="20000")
