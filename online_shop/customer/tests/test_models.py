d1a9914373bd6c8a426d47441f4a2f1b7329a77f
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-15 10:46:54 +0330

    Added tests to AddressTest

--- online_shop/customer/tests/test_models.py
+++ online_shop/customer/tests/test_models.py
@@ -9,10 +9,12 @@ class CustomerTest(TestCase):
                                                 last_name="bolouk",
                                                 birth_date=date(1997, 5, 21))
 
-    def test1_customer1_name(self):
+    def test1_customer_name(self):
         self.assertEqual("amir", self.customer.first_name)
         self.assertEqual("bolouk", self.customer.last_name)
         self.assertEqual("amir bolouk", self.customer.full_name())
+
+    def test2_customer_birthdate(self):
         self.assertEqual(1997, self.customer.birth_date.year)
         self.assertEqual(5, self.customer.birth_date.month)
         self.assertEqual(21, self.customer.birth_date.day)
@@ -37,3 +39,16 @@ class AddressTest(TestCase):
         self.address3 = Address.objects.create(province="فارس",
                                                city="شیراز",
                                                customer=self.customer1)
+
+    def test3_address1_details(self):
+        self.assertEqual("تهران", self.address1.province)
+        self.assertEqual("تهران", self.address1.city)
+        self.assertEqual("میدان انقلاب - کارگر شمالی - کوچه ۵- ساختمان اکبر- طبقه منفی ۸", self.address1.detail)
+        self.assertEqual(self.customer2, self.address1.customer)
+
+    def test4_address3_detail(self):
+        self.assertEqual("", self.address3.detail)
+
+    def test5_customer1_addresses(self):
+        # self.assertIn(,self.customer1.address_set)
+        print(self.customer1.address_set)
