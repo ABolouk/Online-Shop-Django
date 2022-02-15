bc0e968b1b7ca9e40cf94c8d80e33ceb4d1aa4fa
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-15 14:17:26 +0330

    registered product models to admin

--- online_shop/product/admin.py
+++ online_shop/product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from product.models import Category, Discount, DiscountCode, Brand, Product
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Discount)
+admin.site.register(DiscountCode)
+admin.site.register(Brand)
+admin.site.register(Product)
