ff48070aec8f71f5df2b0464ac2648995bc071e4
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-15 13:07:31 +0330

    Added price method to Order

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -9,6 +9,16 @@ class Order(models.Model):
     customer = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
     discount_code = models.ForeignKey(to=DiscountCode, null=True, on_delete=models.SET_NULL)
 
+    def total_price(self):
+        price = 0
+        for item in self.orderitem_set.all():
+            price += int(item.price())
+
+        # TODO Add discount method, or move final price method in product to core.utils and use it here
+
+    def __str__(self):
+        return f"{self.customer} {self.price()}"
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(to=Order, on_delete=models.CASCADE)
@@ -16,7 +26,7 @@ class OrderItem(models.Model):
     number = models.IntegerField(validators=[MinValueValidator(1), ])
 
     def price(self):
-        return str(self.number * int(self.product.price))
+        return str(self.number * int(self.product.final_price()))
 
     def __str__(self):
         return f"{self.number} {self.product.name}"
