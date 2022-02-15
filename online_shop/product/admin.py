37d770bab946cfbf0a85796f87ab638e2753b9ef
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-15 15:02:14 +0330

    modified for OffCode

--- online_shop/product/admin.py
+++ online_shop/product/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from product.models import Category, Discount, DiscountCode, Brand, Product
+from product.models import Category, Discount, OffCode, Brand, Product
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Discount)
-admin.site.register(DiscountCode)
+admin.site.register(OffCode)
 admin.site.register(Brand)
 admin.site.register(Product)
