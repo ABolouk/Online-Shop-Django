d81aaceed3fd5f8327143e20d3e2add7df6f768e
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-15 11:04:41 +0330

    Added DiscountTest

--- online_shop/product/tests/test_models.py
+++ online_shop/product/tests/test_models.py
@@ -1,2 +1,15 @@
 from django.test import TestCase
 from product.models import Product, Discount, DiscountCode, Category
+
+
+class DiscountTest(TestCase):
+    def setUp(self) -> None:
+        self.discount = Discount.objects.create(type="PER",
+                                                amount="40",
+                                                max_value="20000")
+
+    def test1_discount_detail(self):
+        self.assertEqual("PER", self.discount.type)
+        self.assertFalse("per" == self.discount.type)
+        self.assertEqual("40", self.discount.amount)
+        self.assertEqual("20000", self.discount.max_value)
