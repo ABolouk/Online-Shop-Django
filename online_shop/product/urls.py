0d937ab9e0cd19ba2ba5165de43394a9a1ccc665
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-17 15:05:58 +0330

    added product_list_view

--- online_shop/product/urls.py
+++ online_shop/product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import product_list_view
 
 urlpatterns = [
-
+    path("list_view/", product_list_view)
 ]
--- online_shop/product/views.py
+++ online_shop/product/views.py
@@ -1,3 +1,27 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+
+from product.models import Product
+from product.serializers import ProductSerializer
+
 
 # Create your views here.
+
+def product_list_view(request):
+    if request.method == "POST":
+        data = request.POST
+        product_deserializer = ProductSerializer(data=data)
+        if product_deserializer.is_available():
+            new_product = product_deserializer.save()
+            return JsonResponse({'new_car_id': new_product.id}, status=201)
+        else:
+            JsonResponse({"errors": product_deserializer.errors}, status=400)
+
+    elif request.method == "GET":
+        product_serializer = ProductSerializer(instance=Product.objects.all(), many=True)
+        print(product_serializer.data)
+        return JsonResponse({"data": product_serializer.data}, status=200)
+
+    else:
+        return JsonResponse({"msg": "invalid method!"}, status=405)
