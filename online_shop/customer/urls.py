3bcfafaff89d578924c981752adbfc4c380139da
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 15:55:11 +0330

    added customer profile url

--- online_shop/customer/urls.py
+++ online_shop/customer/urls.py
@@ -3,9 +3,9 @@ from customer import views
 
 app_name = "customer"
 urlpatterns = [
-    # path("order_history/", views.OrderHistory.as_view(), name="order-history"),
-    # path("order_status/", views.OrderHistory.as_view(), name="order-status"),
-    # path("address/", views.OrderHistory.as_view(), name="address"),
-    # path("profile/", views.OrderHistory.as_view(), name="profile"),
+    # path("order_history/", views.OrderHistoryView.as_view(), name="order-history"),
+    # path("order_status/", views.OrderStatusView.as_view(), name="order-status"),
+    # path("address/", views.AddressView.as_view(), name="address"),
+    path("profile/", views.ProfileView.as_view(), name="profile"),
     # path("profile/update/", views.OrderHistory.as_view(), name="profile-update"),
 ]
