01b6cf8d821da677f2adc85fca0923c9555da9a3
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 14:10:06 +0330

    added post to CustomerRegister

--- online_shop/core/views.py
+++ online_shop/core/views.py
@@ -1,19 +1,36 @@
+from django.contrib.auth.models import Group
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from django.views import View
 from customer.models import Customer
+from core.models import User
 from customer.forms import CustomerRegisterForm, UserForm
 
 
 class CustomerRegister(View):
     def get(self, request):
-        user_form = CustomerRegisterForm()
-        customer_form = UserForm()
+        form = UserForm()
         context = {
-            "user_form": user_form,
-            "customer_form": customer_form,
+            "form": form,
         }
         return render(request=request, template_name="customer/customer_register.html", context=context)
+
+    def post(self, request):
+        form = UserForm(request.POST)
+        if form.is_valid():
+            phone = request.POST['phone']
+            password = request.POST['password']
+            user: User = User.objects.create_user(phone=phone, password=password)
+            customer = Customer.objects.create(user=user)
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            return HttpResponse(f"{customer}")
+        else:
+            context = {
+                "form": form,
+            }
+            return render(request=request, template_name="customer/customer_register.html", context=context)
 
 
 class CustomerLogin(View):
