19099fac4f4a9113d4574b5c55825b62e096b786
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 04:35:29 +0330

    modified MyUserManager

--- online_shop/core/models.py
+++ online_shop/core/models.py
@@ -3,11 +3,6 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class MyUserManager(UserManager):
-
-    def _create_user(self, username, email, password, **extra_fields):
-        self.username = extra_fields["phone"]
-        return super()._create_user(username, email, password, **extra_fields)
-
     def create_superuser(self, username=None, email=None, password=None, **extra_fields):
         username = extra_fields["phone"]
         return super().create_superuser(username, email, password, **extra_fields)
