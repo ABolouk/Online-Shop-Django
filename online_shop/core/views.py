f99234e1cafa1030d2d0713c099226605715dab9
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 08:33:23 +0330

    added CustomerRegister and CustomerLogin to core.views

--- online_shop/core/views.py
+++ online_shop/core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+from customer.models import Customer
+from customer.forms import CustomerRegisterForm, UserForm
+
+
+class CustomerRegister(View):
+    def get(self, request):
+        user_form = CustomerRegisterForm()
+        customer_form = UserForm()
+        context = {
+            "user_form": user_form,
+            "customer_form": customer_form,
+        }
+        return render(request=request, template_name="customer/customer_register.html", context=context)
+
+
+class CustomerLogin(View):
+    pass
