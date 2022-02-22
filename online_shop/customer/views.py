b3dbc86fe1b1a2404a1e151dd390d022d8179235
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 16:43:54 +0330

    added AddressView

--- online_shop/customer/views.py
+++ online_shop/customer/views.py
@@ -8,16 +8,22 @@ from customer.models import Customer
 
 
 class ProfileView(PermissionRequiredMixin, View):
-    permission_required = ["being_customer", ]
+    permission_required = "customer.being_customer"
 
     def get(self, request):
         if not request.user.is_authenticated:
-            return HttpResponseRedirect(reverse("login"))
+            # TODO: add an error message
+            return HttpResponseRedirect(reverse("login-customer"))
         user = request.user
-        print(user)
-        print(user.id)
         customer = Customer.objects.get(user_id=user.id)
         context = {
             "customer": customer,
         }
         return render(request=request, template_name="customer/customer_profile.html", context=context)
+
+
+class AddressView(PermissionRequiredMixin, View):
+    permission_required = "customer.being_customer"
+
+    def get(self, request):
+        pass
