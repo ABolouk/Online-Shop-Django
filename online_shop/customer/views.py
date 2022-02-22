c502b8cd2318d94cb48b26e0f91bb56bda09f260
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 15:54:40 +0330

    added ProfileView

--- online_shop/customer/views.py
+++ online_shop/customer/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse
 
-# Create your views here.
+from django.views import View
+from customer.models import Customer
+
+
+class ProfileView(PermissionRequiredMixin, View):
+    permission_required = ["being_customer", ]
+
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("login"))
+        user = request.user
+        print(user)
+        print(user.id)
+        customer = Customer.objects.get(user_id=user.id)
+        context = {
+            "customer": customer,
+        }
+        return render(request=request, template_name="customer/customer_profile.html", context=context)
