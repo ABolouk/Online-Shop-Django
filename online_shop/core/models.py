461b1629f0c3fe767ae3f8d00f45570efa6aba54
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 04:25:44 +0330

    modified create_superuser for MyUserManager

--- online_shop/core/models.py
+++ online_shop/core/models.py
@@ -5,12 +5,16 @@ from django.contrib.auth.models import AbstractUser, UserManager
 class MyUserManager(UserManager):
 
     def _create_user(self, username, email, password, **extra_fields):
-        self.username = extra_fields['phone']
+        self.username = extra_fields["phone"]
         return super()._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+        username = extra_fields["phone"]
+        return super().create_superuser(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
     objects = MyUserManager()
 
     phone = models.CharField(max_length=15, unique=True)
-    USERNAME_FIELD = 'phone'
+    USERNAME_FIELD = "phone"
