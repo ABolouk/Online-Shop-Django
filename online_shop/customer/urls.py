595784d383d9ada5d697473fd99348558f74aba6
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 07:40:13 +0330

    pre-added urls for customer app

--- online_shop/customer/urls.py
+++ online_shop/customer/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from customer import views
 
+app_name = "customer"
 urlpatterns = [
-
+    # path("order_history/", views.OrderHistory.as_view(), name="order-history"),
+    # path("order_status/", views.OrderHistory.as_view(), name="order-status"),
+    # path("address/", views.OrderHistory.as_view(), name="address"),
+    # path("profile/", views.OrderHistory.as_view(), name="profile"),
+    # path("profile/update/", views.OrderHistory.as_view(), name="profile-update"),
 ]
