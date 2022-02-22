7f47c1afd9d6041f9668fcb3de1b78974e9d7d0a
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 16:44:31 +0330

    commented app name in customer.urls

--- online_shop/customer/urls.py
+++ online_shop/customer/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from customer import views
 
-app_name = "customer"
+# app_name = "customer"
 urlpatterns = [
     # path("order_history/", views.OrderHistoryView.as_view(), name="order-history"),
     # path("order_status/", views.OrderStatusView.as_view(), name="order-status"),
-    # path("address/", views.AddressView.as_view(), name="address"),
+    path("address/", views.AddressView.as_view(), name="address"),
     path("profile/", views.ProfileView.as_view(), name="profile"),
     # path("profile/update/", views.OrderHistory.as_view(), name="profile-update"),
 ]
