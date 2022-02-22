9e1ead95b4b30b89b6619606c25f030b11996968
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 15:56:03 +0330

    changed LoginForm to Form

--- online_shop/core/forms.py
+++ online_shop/core/forms.py
@@ -4,17 +4,22 @@ from django.utils.translation import gettext_lazy as _
 from core.models import User
 
 
-class LoginForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ["phone", "password"]
+# class LoginForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ["phone", "password"]
+#
+#         labels = {
+#             'phone': _("Phone Number"),
+#             'password': _("Password"),
+#         }
+#
+#         widgets = {
+#             'phone': forms.TextInput(attrs={'placeholder': _("Example: 09123456789")}),
+#             'password': forms.PasswordInput(),
+#         }
 
-        labels = {
-            'phone': _("Phone Number"),
-            'password': _("Password"),
-        }
 
-        widgets = {
-            'phone': forms.TextInput(attrs={'placeholder': _("Example: 09123456789")}),
-            'password': forms.PasswordInput(),
-        }
+class LoginForm(forms.Form):
+    phone = forms.CharField(max_length=15, label=_("Phone"))
+    password = forms.CharField(max_length=100, label=_("Password"), widget=forms.PasswordInput)
