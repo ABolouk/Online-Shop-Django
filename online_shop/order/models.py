d32d17f1a1c651b3e7a28a3507b0e09505369d7a
Bolouk <amirhosseinbolouk@gmail.com>
2022-02-22 05:33:02 +0330

    modified Order

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -1,23 +1,29 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator
 
+from core.models import BaseModel
+from core.utils import price_discount
 from product.models import Product, OffCode
 from customer.models import Customer
-from core.utils import price_discount
 
 
-class Order(models.Model):
-    customer = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
-    discount_code = models.ForeignKey(to=OffCode, null=True, blank=True, on_delete=models.SET_NULL)
+class Order(BaseModel):
+    customer = models.ForeignKey(to=Customer, on_delete=models.RESTRICT)
+    off_code = models.ForeignKey(to=OffCode, null=True, blank=True, on_delete=models.SET_NULL)
 
     def total_price(self):
         price = 0
         for item in self.orderitem_set.all():
             price += int(item.price())
-        return price_discount(price, self.discount_code)
+        return price_discount(price, self.off_code)
 
     def __str__(self):
         return f"{self.customer} {self.total_price()}"
+
+    class Meta:
+        verbose_name = _("Order")
+        verbose_name_plural = _("Orders")
 
 
 class OrderItem(models.Model):
