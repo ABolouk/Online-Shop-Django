93e6a3eede3e5b897aae2d0c8cbead19293cfe6c
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-03 20:57:56 +0430

    added AddAddressView

--- online_shop/customer/views.py
+++ online_shop/customer/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from rest_framework import generics, permissions, authentication
 
 from django.views import View
-from django.views.generic import UpdateView, ListView, DetailView
+from django.views.generic import UpdateView, ListView, DetailView, CreateView
 
 from core.models import User
 from customer.models import Customer, Address
@@ -137,3 +137,19 @@ class PasswordChangeView(PermissionRequiredMixin, View):
             messages.error(request, _("Enter a valid password."), "danger")
             return render(request=request, template_name="customer/change_password.html", context=context)
 
+
+class AddAddressView(PermissionRequiredMixin, CreateView):
+    permission_required = "customer.being_customer"
+
+    model = Address
+    fields = ["province", "city", "detail"]
+    template_name = "customer/add_address.html"
+    success_url = reverse_lazy("customer:address")
+
+    def form_valid(self, form):
+        address = form.save(commit=False)
+        customer = Customer.objects.get(user=self.request.user)
+        address.customer = customer
+        address.save()
+        messages.success(self.request, _("Address successfully added."), "success")
+        return super().form_valid(form)
