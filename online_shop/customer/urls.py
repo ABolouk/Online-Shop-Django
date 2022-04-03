216945b645567f47509246a2fcd67fca79bc834e
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-03 20:01:37 +0430

    added urls for add and edit address and password change

--- online_shop/customer/urls.py
+++ online_shop/customer/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path("order_history/<int:pk>", views.OrderHistoryListView.as_view(), name="order-items"),
     # path("order_status/", views.OrderStatusView.as_view(), name="order-status"),
     path("address/", views.AddressView.as_view(), name="address"),
+    path("address/edit/<int:address_id>", views.AddAddressView.as_view(), name="add-address"),
+    path("address/add/", views.EditAddressView.as_view(), name="edit-address"),
     path("profile/", views.ProfileView.as_view(), name="profile"),
     path("profile/update/", views.ProfileUpdateView.as_view(), name="profile-update"),
-
+    path("profile/password/change/", views.PasswordChangeView.as_view(), name="password-change"),
     path("address_api/<int:pk>", views.AddressDetailView.as_view()),
     path("address_list_api/", views.AddressListView.as_view()),
 ]
