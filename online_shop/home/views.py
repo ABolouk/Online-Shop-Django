07afc8f3e32381906d4ae89e68788053ced90ce8
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-06 17:37:23 +0430

    added categories to home view

--- online_shop/home/views.py
+++ online_shop/home/views.py
@@ -1,13 +1,15 @@
 from django.views import View
 from django.shortcuts import render
 
-from product.models import Product
+from product.models import Product, Category
 
 
 class HomeView(View):
     def get(self, request):
         products = Product.objects.all()
+        categories = Category.parents()
         context = {
-            "products": products
+            "products": products,
+            "categories": categories,
         }
         return render(request, template_name="product/home.html", context=context)
