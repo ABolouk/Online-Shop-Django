8be3db1d0bbc6c49ba42e99fbbaf4451d6f2b969
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-06 22:56:23 +0430

    Added ModelAdmin

--- online_shop/customer/admin.py
+++ online_shop/customer/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from customer.models import Customer, Address
 
-# Register your models here.
-admin.site.register(Customer)
-admin.site.register(Address)
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'province', 'city', 'detail')
+    list_filter = ('province', 'city')
+    search_fields = ('province', 'city', 'detail')
+    raw_id_fields = ('customer',)
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Address, AddressAdmin)
