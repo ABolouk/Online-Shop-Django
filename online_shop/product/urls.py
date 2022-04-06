8807d5cd8051541a87eb97ca5604c9107e556a09
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-07 02:58:15 +0430

    added category and brand url

--- online_shop/product/urls.py
+++ online_shop/product/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from . import api_views
 from . import views
 
 app_name = "product"
 urlpatterns = [
+    path('category/<int:category_id>/', views.CategoryProductView.as_view(), name="category"),
+    path('brand/<int:brand_id>/', views.BrandProductView.as_view(), name="brand"),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name="product_detail"),
+    path('api/list/', api_views.ProductListAPI.as_view(), name="list_api"),
     # path("list_api/", views.ProductListAPI.as_view(), name="product_list_api"),
     # path("api/<int:pk>", views.ProductDetailAPI.as_view(), name="product_detail_api"),
 ]
