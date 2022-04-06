b93539ceb70da1e99f052843c553a31baa5093ec
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-07 02:58:41 +0430

    bugfix: removed unused url

--- online_shop/order/urls.py
+++ online_shop/order/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "order"
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
+
 ]
