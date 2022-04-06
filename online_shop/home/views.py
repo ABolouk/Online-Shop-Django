9c61ff5883abfa2d9070774e6df070db9dd89a11
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-07 02:57:52 +0430

    added brands to HomeView Context

--- online_shop/home/views.py
+++ online_shop/home/views.py
@@ -1,15 +1,17 @@
 from django.views import View
 from django.shortcuts import render
 
-from product.models import Product, Category
+from product.models import Product, Category, Brand
 
 
 class HomeView(View):
     def get(self, request):
         products = Product.objects.all()
         categories = Category.parents()
+        brands = Brand.objects.all()
         context = {
             "products": products,
             "categories": categories,
+            "brands": brands,
         }
         return render(request, template_name="product/home.html", context=context)
