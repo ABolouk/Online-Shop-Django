33f2771e8f7309ff2b931be86385920860f61756
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-08 02:57:54 +0430

    completed CartView

--- online_shop/home/views.py
+++ online_shop/home/views.py
@@ -1,7 +1,9 @@
 from django.views import View
 from django.shortcuts import render
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 
 from product.models import Product, Category, Brand
+from order.models import Order, OrderItem
 
 
 class HomeView(View):
@@ -17,12 +19,29 @@ class HomeView(View):
         return render(request, template_name="product/home.html", context=context)
 
 
-class CartView(View):
+class CartView(PermissionRequiredMixin, View):
+    permission_required = "customer.being_customer"
+
     def get(self, request):
+        customer = request.user.customer
+        addresses = customer.address_set.all()
+        try:
+            order = customer.order_set.get(is_open=True)
+        except:
+            order = Order.objects.create(customer=customer, address=customer.address_set.first())
+            data: dict = request.session.get("cart", {})
+            for product_id, data in data.items():
+                number = data["number"]
+                product = Product.objects.get(id=product_id)
+                order_item = OrderItem.objects.create(order=order, product=product, number=number)
+            order.save()
+
         products = Product.objects.all()
         categories = Category.parents()
         brands = Brand.objects.all()
         context = {
+            "order": order,
+            "addresses": addresses,
             "products": products,
             "categories": categories,
             "brands": brands,
