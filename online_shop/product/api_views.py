01590e4eb7d91674bf16562abc8fc37266c51927
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-07 23:17:30 +0430

    added AddToCart to api_views

--- online_shop/product/api_views.py
+++ online_shop/product/api_views.py
@@ -1,5 +1,10 @@
+from rest_framework.response import Response
 from rest_framework import generics, authentication
+from rest_framework.views import APIView
+from django.utils.translation import gettext_lazy as _
+
 from .models import Product
+from order.models import OrderItem, Order
 from .serializers import ProductSerializer
 
 
@@ -9,3 +14,21 @@ class ProductListAPI(generics.ListAPIView):
     queryset = Product.objects.filter(is_available=True)
     authentication = [authentication.BasicAuthentication]
     # permissions = [IsSuperuserPermission]
+
+
+class AddToCart(APIView):
+    authentication = [authentication.BasicAuthentication]
+
+    def get(self, request):
+        product_id = request.query_params.get("product_id")
+        number = request.query_params.get("number")
+        product = Product.objects.get(id=product_id)
+        serializer = ProductSerializer(instance=product)
+        try:
+            data = request.session["cart"]
+            data.update({product_id: {"number": number, "name": Product.objects.get(id=product_id).name}})
+            request.session.pop("cart")
+            request.session["cart"] = data
+        except:
+            request.session["cart"] = {product_id: {"number": number, "name": Product.objects.get(id=product_id).name}}
+        return Response(data=serializer.data)
