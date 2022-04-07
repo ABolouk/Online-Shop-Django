150c185b809ff44ce14843305718e91a4a4ab33f
Bolouk <amirhosseinbolouk@gmail.com>
2022-04-07 23:14:50 +0430

    added cart related urls

--- online_shop/product/urls.py
+++ online_shop/product/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('brand/<int:brand_id>/', views.BrandProductView.as_view(), name="brand"),
     path('<int:pk>/', views.ProductDetailView.as_view(), name="product_detail"),
     path('api/list/', api_views.ProductListAPI.as_view(), name="list_api"),
+    path('api/add_to_cart/', api_views.AddToCart.as_view(), name="add_to_cart"),
+    path('get_cart/', api_views.GetCart.as_view(), name="get_cart"),
+    path('delet_from_cart/', api_views.DeleteFromCart.as_view(), name="delete_from_cart"),
     # path("list_api/", views.ProductListAPI.as_view(), name="product_list_api"),
     # path("api/<int:pk>", views.ProductDetailAPI.as_view(), name="product_detail_api"),
 ]
